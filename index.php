3516d2da4d7673f6ce600c7d784d4b4bb59160e9
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-04-04 17:00:29 +0900

    Form preparation commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <div class="header">
+    <div class="header-left">Progate</div>
+    <div class="header-right">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li class="selected">お問い合わせ</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="main">
+    <div class="contact-form">
+      <div class="form-title">お問い合わせ</div>
+      <form action="sent.php"  method="post">
+    </div>
+  </div>
+</body>
+</form>
