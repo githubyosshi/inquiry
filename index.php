ccb4fce4d22a8a25242b6c0a407df69604670002
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-04-07 20:57:52 +0900

    Transmission function added commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Progate</title>
-  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 </head>
 <body>
   <div class="header">
@@ -20,8 +20,32 @@
   <div class="main">
     <div class="contact-form">
       <div class="form-title">お問い合わせ</div>
-      <form action="sent.php"  method="post">
+      <!-- post(送信されるurl非表示)かget(url表示) -->
+      <!-- <form action="sent.php"  method="post"> -->
+      <form method="post" action="sent.php">
+        <div class="form-item">名前</div>
+        <input type="text" name="name">
+        <!-- <div class="form-item">Emailを入力して下さい</div>
+        <input type="text" name="email"> -->
+        <div class="form-item">内容</div>
+        <!-- <textarea name="content"></textarea> -->
+        <textarea name="body"></textarea>
+        <input type="submit" value="送信">
+      </form>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="footer-left">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li>お問い合わせ</li>
+      </ul>
+    </div>
+    <div class="like-box">
+      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
     </div>
   </div>
 </body>
-</form>
+</html>
